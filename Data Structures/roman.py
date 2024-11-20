ca5a5b4e35c56928ce8bb288dfa70986d83552aa
Felipe Daniel Juarez Alvarez <spider_spainfel@hotmail.com>
2024-11-20 09:56:50 -0600

    finish roman problem

--- Data Structures/roman.py
+++ Data Structures/roman.py
@@ -1,34 +1,29 @@
 class Solution:
     def __init__(self):
         self.roman = {
-        'I': 1, 
-        'V': 5, 
-        'X': 10, 
-        'L': 50, 
-        'C': 100, 
-        'D': 500, 
-        'M': 1000, 
-        'IV': 4,  # 4 = 5 - 1
-        'IX': 9,  # 9 = 10 - 1
-        'XL': 40, # 40 = 50 - 10
-        'XC': 90, # 90 = 100 - 10
-        'CD': 400, # 400 = 500 - 100
-        'CM': 900  # 900 = 1000 - 100
-    }
-
+            'I': 1, 
+            'V': 5, 
+            'X': 10, 
+            'L': 50, 
+            'C': 100, 
+            'D': 500, 
+            'M': 1000, 
+            'IV': 4, 
+            'IX': 9, 
+            'XL': 40, 
+            'XC': 90, 
+            'CD': 400, 
+            'CM': 900
+        }
 
     def romanToInt(self, s: str) -> int:
         total = 0
-        value2 = 0
-        for count, value in enumerate(s):
-            if count < len(s) - 1:
-                next_value = s[count + 1]
-                value2 = value + next_value
-                # print(value2)
-            if value2 in self.roman:
-                total += self.roman[value2]
-                print(self.roman[value2])
-            elif value in self.roman:
-                total += self.roman[value]
-                print(self.roman[value])
+        i = 0
+        while i < len(s):
+            if i < len(s) - 1 and s[i:i + 2] in self.roman:
+                total += self.roman[s[i:i + 2]]
+                i += 2 
+            else:
+                total += self.roman[s[i]]
+                i += 1
         return total
