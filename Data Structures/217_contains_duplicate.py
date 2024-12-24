c2fbae29404746c743333405b54ecc6f735a8f7a
Felipe Daniel Juarez Alvarez <spider_spainfel@hotmail.com>
2024-12-24 12:00:05 -0600

    finish problem 217

---
+++ Data Structures/217_contains_duplicate.py
@@ -0,0 +1,10 @@
+# Se puede hacer lo mismo pero con un diccionario
+# Son muy similares los resultados
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
