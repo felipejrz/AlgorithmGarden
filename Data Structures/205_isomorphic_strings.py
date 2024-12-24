a02d92b93c83b334a73fa0ac5257896b96b57d43
Felipe Daniel Juarez Alvarez <spider_spainfel@hotmail.com>
2024-12-24 08:24:51 -0600

    finish problem 205

--- Data Structures/205_isomorphic_strings.py
+++ Data Structures/205_isomorphic_strings.py
@@ -1,14 +1,25 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        # Diccionario para mapear caracteres de 's' a 't'
         dic = {}
+
+        # Si las longitudes no coinciden, no pueden ser isomorfos
         if len(s) != len(t):
             return False
+
+        # Iterar sobre cada carácter de las cadenas
         for i in range(len(s)):
-            if not s[i] in dic:
+            # Si el carácter de 's' no está en el diccionario
+            if s[i] not in dic:
+                # Verificar que el carácter de 't' no haya sido asignado a otro carácter
+                if t[i] in dic.values():
+                    return False
+                # Asignar la relación s[i] -> t[i] en el diccionario
                 dic[s[i]] = t[i]
-                # print(dic[s[i]])
             else:
+                # Si el carácter ya está mapeado, verificar que coincida con el carácter actual de 't'
                 if dic[s[i]] != t[i]:
                     return False
-        # print(dic)
+
+        # Si pasamos todas las verificaciones, las cadenas son isomorfas
         return True
