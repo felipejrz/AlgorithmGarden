8a43d0687e72c3215e3612c5c11ba6e902b8e5d7
Felipe Daniel Juarez Alvarez <spider_spainfel@hotmail.com>
2025-01-02 10:09:24 -0600

    finish problmen 3

---
+++ Data Structures/03_longest_substring_without_repeating_characters.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        dic = {}
+        max_len = 0
+        start = 0  # Puntero
+
+        for i in range(len(s)):
+            if s[i] in dic and dic[s[i]] >= start:
+                # Si el carácter está repetido y está dentro de la subcadena actual
+                start = dic[s[i]] + 1  # Ajusta el inicio justo después de la repetición
+
+            dic[s[i]] = i  # Actualiza el índice del carácter
+            max_len = max(max_len, i - start + 1)  # Calcula la longitud máxima
+
+        return max_len
