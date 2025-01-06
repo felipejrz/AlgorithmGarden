7d0a1a0534e96c3488d004a83ec4ae77b8d85be3
Felipe Daniel Juarez Alvarez <spider_spainfel@hotmail.com>
2025-01-06 09:24:12 -0600

    Edit problem 231

--- Data Structures/231_power_of_two.py
+++ Data Structures/231_power_of_two.py
@@ -15,9 +15,21 @@
 
 # Esta es la solucion mas eficiente
 
-
 # Un número es potencia de 2 si en su representación binaria solo tiene un bit establecido en 1.
 # n&(n−1) borra el bit menos significativo, y será igual a 0 solo si n es una potencia de 2.
+# class Solution:
+#     def isPowerOfTwo(self, n: int) -> bool:
+#         return n > 0 and (n & (n - 1)) == 0
+
+
+# Solucion que si entiendo y eficiente
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        return n > 0 and (n & (n - 1)) == 0
+        # Checar si es mayor a 0
+        if n <= 0:
+            return False
+        # Mientras sea divisible entre 2 hacer el bucle
+        while n % 2 == 0:
+            n //= 2
+        # Si es 1 regresamos True
+        return n == 1
